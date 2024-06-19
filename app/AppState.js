6e484c0658141cbd10dc9d53b2a6037237f8d4b8
jo <josehehall98@gmail.com>
2024-06-18 23:35:34 -0600

    Style the cans

--- app/AppState.js
+++ app/AppState.js
@@ -13,19 +13,37 @@ class ObservableAppState extends EventEmitter {
       'Orange Drink',
       1.25,
       'text-warning',
-      'https://media.istockphoto.com/id/471669985/photo/orange-drink-can-with-clipping-path.jpg?s=1024x1024&w=is&k=20&c=H5XtIxsNoMcL87PH-E-Vc0H1wWQOBTDgt_hem8QVQtA='
+      'https://media.istockphoto.com/id/951951548/vector/aluminum-can-mockup-vector-illustration.jpg?s=612x612&w=0&k=20&c=SGrSzernA5YglnfJaVMwkrZYbWpEiby1ItVzv1YZXME='
     ),
     new VenderItems(
       'Blue Drink',
       2.25,
       'text-primary',
-      'https://media.istockphoto.com/id/176518144/photo/blue-drink-can-with-clipping-path.jpg?s=1024x1024&w=is&k=20&c=DFOLALE0ehi4baFxT7OP4xgfxRmy5ty_0Yh8vlhRcI8='
+      'https://media.istockphoto.com/id/947674080/vector/aluminum-can-mockup-vector-illustration.jpg?s=612x612&w=0&k=20&c=kQvuUzr8IMaRMpCnXslLE8oMmSwgtP1wGrMY78YKKmI='
     ),
     new VenderItems(
       'Red Drink',
       0.25,
       'text-danger',
-      'https://media.istockphoto.com/id/175113989/photo/red-drink-can-with-clipping-path.jpg?s=1024x1024&w=is&k=20&c=nz_R1RYAVWsJ0_Oen9h1mqcAFS4LsaV5ifgtJZdkQGA='
+      'https://media.istockphoto.com/id/952095350/vector/aluminum-can-mockup-vector-illustration.jpg?s=612x612&w=0&k=20&c=1oQa5j6VJCilQNVY-F0WZnFWjvMF91XYxqe4XVOYP-g='
+    ),
+    new VenderItems(
+      'Green Drink',
+      4.25,
+      'text-success',
+      'https://media.istockphoto.com/id/951604202/vector/aluminum-can-mockup-vector-illustration.jpg?s=612x612&w=0&k=20&c=l6DLDf_fXmmrUbcRINUHZ1_jbDVs6g0ETi5DGYeFHN4='
+    ),
+    new VenderItems(
+      'White Drink',
+      0.50,
+      'text-light',
+      'https://media.istockphoto.com/id/945123880/vector/aluminum-can-mockup-vector-illustration.jpg?s=612x612&w=0&k=20&c=dao3tXtIw6BypaxvToocI2eCewWdv-WBYb9el70vgbE='
+    ),
+    new VenderItems(
+      "Black Drink",
+      10.00,
+      'text-black',
+      'https://media.istockphoto.com/id/952995002/vector/aluminum-can-mockup-vector-illustration.jpg?s=612x612&w=0&k=20&c=3iXePz5XKstm4B769gMLuEJavGxNx7Rl2_7wt0YQWSc='
     )
   ]
   myMoney = 0.25
--- app/models/VenderItems.js
+++ app/models/VenderItems.js
@@ -12,10 +12,9 @@ export class VenderItems {
         return /*html*/ `
             <div class="col-12 col-md-4">
                 <div class="card bg-black d-flex justify-content-between align-items-center m-2 text-light">
-                    <img class="img-thumbnail" src="${this.img}" alt="">
+                    <img onclick="app.MoneyController.spendMoney('${this.name}')" class="img-thumbnail" src="${this.img}" alt="">
                     <p class="${this.textColor}">${this.name}</p>
                     <p>$${this.cost}</p>
-                    <button onclick="app.MoneyController.spendMoney('${this.name}')">153</button>
             </div>
         </div>
         `
